16b8b27321b9313dc22de20aea8c2d68fd1c78a2
ictlyh <luoyuanhao@software.ict.ac.cn>
2015-05-09 19:28:36 +0800

    实现：Similarity

--- Similarity.cpp
+++ Similarity.cpp
@@ -34,11 +34,31 @@ float Similarity::getShapeWeight()
 
 float Similarity::similarity(Histogram a, Histogram b)
 {
-
-	return 0.0;
+	float molecule = 0.0;
+	float denominator = 0.0;
+	for(int i = 0; i < a.getDim(); i++)
+	{
+		if(a.getFeature(i) > b.getFeature(i))
+			molecule += b.getFeature(i);
+		else
+			molecule += a.getFeature(i);
+		denominator += b.getFeature(i);
+	}
+	return molecule / denominator;
 }
 
 float Similarity::similarity(Image a, Image b)
 {
-	return 0.0;
+	float colorSim = 0.0;
+	float shapeSim = 0.0;
+	ColorFeature aColor = a.getFeature().getColorFeature();
+	ColorFeature bColor = b.getFeature().getColorFeature();
+	ShapeFeature aShape = a.getFeature().getShapeFeature();
+	ShapeFeature bShape = b.getFeature().getShapeFeature();
+	colorSim = similarity(aColor.getH(), bColor.getH())
+		+ similarity(aColor.getS(), bColor.getS())
+		+ similarity(aColor.getV(), bColor.getV());
+	shapeSim = similarity(aShape.getHorizontal(), bShape.getHorizontal())
+		+ similarity(aShape.getVertical(), bShape.getVertical());
+	return colorWeight * colorSim + shapeWeight * shapeSim;
 }
