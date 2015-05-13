a8dd6ed877c601935c95a2c1ee6841029b5e54de
ictlyh <luoyuanhao@software.ict.ac.cn>
2015-05-13 11:51:57 +0800

    完成图像库目录和文件路径设置逻辑 完成检索图像设置逻辑 Issue：图像库目录设置乱码

--- cbir.cpp
+++ cbir.cpp
@@ -8,6 +8,9 @@ CBIR::CBIR(QWidget *parent) :
   ui(new Ui::CBIR)
 {
     ui->setupUi(this);
+    queryImage.setPath("");
+    imageLib.setLibDir("");
+    results.clear();
 }
 
 CBIR::~CBIR()
@@ -59,7 +62,10 @@ void CBIR::showResults()
 void CBIR::on_pBLibDir_clicked()
 {
     QString libDirectory = QFileDialog::getExistingDirectory(this, QString::fromLocal8Bit("选择图像库目录"),QString::fromLocal8Bit("."));
-    //QDebug << "The lib directory is :" << libDirectory << endl;
+    // Press Cancle
+    if(libDirectory == NULL)
+      return ;
+    // 为 imageLib.setLibDir 有问题
     imageLib.setLibDir((char*)libDirectory.toStdString().c_str());
     ui->leLibDir->setText(libDirectory);
 }
@@ -67,6 +73,9 @@ void CBIR::on_pBLibDir_clicked()
 void CBIR::on_pBLibFile_clicked()
 {
     QString libFile = QFileDialog::getOpenFileName(this, QString::fromLocal8Bit("选择图像库文件"), QString::fromLocal8Bit("Text (*.txt)"));
+    // Press Cancle
+    if(libFile == NULL)
+      return ;
     //QDebug << "The lib file is : " << libFile << endl;
     imageLib.setLibFile((char*)libFile.toStdString().c_str());
     ui->leLibFile->setText(libFile);
@@ -74,19 +83,38 @@ void CBIR::on_pBLibFile_clicked()
 
 void CBIR::on_pBBuildLib_clicked()
 {
+    if(strlen(imageLib.getLibDir()) == 0)
+      {
+        QMessageBox::information(this, QString::fromLocal8Bit("请选择图像库路径"), QString::fromLocal8Bit("请选择图像库路径"));
+        return ;
+      }
     // 构建图像特征库
     imageLib.buildImageLib();
+    QMessageBox::information(this, QString::fromLocal8Bit("构建特征库完成"), QString::fromLocal8Bit("构建特征库完成"));
+    // 保存图像特征库
+    imageLib.saveImageLib();
+    QMessageBox::information(this, QString::fromLocal8Bit("图像库规模"), QString("%1").arg(imageLib.getImageList().size()));
 }
 
 void CBIR::on_pBLoadLib_clicked()
 {
     // 加载图像特征库文件
-    imageLib.loadImageLib();
+    if(imageLib.loadImageLib())
+      {
+        QMessageBox::information(this, QString::fromLocal8Bit("加载特征库完成"), QString::fromLocal8Bit("加载特征库完成"));
+      }
+    else
+      {
+        QMessageBox::information(this, QString::fromLocal8Bit("加载特征库失败"), QString(imageLib.getLibFile()));
+      }
 }
 
 void CBIR::on_pBSetQuery_clicked()
 {
     QString query = QFileDialog::getOpenFileName(this, QString::fromLocal8Bit("选择查询图像"), ".", QString::fromLocal8Bit("Images (*.png *.bmp *.jpg *.tif *.GIF )"));
+    // Press Cancle
+    if(query == NULL)
+      return ;
     // 在 lbQueryImage 中显示图像，并构建查询图像
     QImage * img = new QImage();
     if(!img->load(query))
@@ -99,20 +127,26 @@ void CBIR::on_pBSetQuery_clicked()
     ui->lbQueryImage->setPixmap(QPixmap::fromImage(*img));
     queryImage.setPath(query.toStdString());
     queryImage.calcFeature();
+    delete img;
 }
 
 void CBIR::on_pBSearch_clicked()
 {
+    if(strlen(queryImage.getPath()) == 0)
+    {
+       QMessageBox::information(this, QString::fromLocal8Bit("请选择检索图像"), QString::fromLocal8Bit("请选择检索图像"));
+       return ;
+    }
     // 检索并显示结果
     results = searcher.search(queryImage, imageLib);
-    //QDebug << "The result is :" << results << endl;
+    QMessageBox::information(this, QString::fromLocal8Bit("检索结果数目"), QString("%1").arg(results.size()));
     iteBegin = results.begin();
     showResults();
 }
 
 void CBIR::on_pBReSearch_clicked()
 {
-  // 如何获取反馈图像??
+    // 如何获取反馈图像??
     list<string> feedback;
     results = searcher.reSearch(queryImage, feedback, imageLib);
     //QDebug << "The result is :" << results << endl;
